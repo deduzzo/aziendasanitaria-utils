ecda9d344ef82ce9b5d87cc1621797dc63093c4f
Roberto De Domenico @deduzzo <deduzzo@gmail.com>
2022-02-18 13:30:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import {flussoM} from "./src/m/flusso-m.js";
-import {flussoSIAD} from "./src/siad.js.js";
+import {flussoSIAD} from "./src/siad/siad.js";
 
 export const flussiRegioneSicilia = {flussoM, flussoSIAD}
