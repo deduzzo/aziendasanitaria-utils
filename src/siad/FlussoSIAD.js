659098f02005b926ea3ef74c7c34835a4ca6e602
Roberto De Domenico @deduzzo <deduzzo@gmail.com>
2023-01-20 11:20:13 +0100

    reportPrestazioni

--- src/siad/FlussoSIAD.js
+++ src/siad/FlussoSIAD.js
@@ -1,89 +1,104 @@
 import * as xml2js from 'xml2js'
 import fs from 'fs';
 import reader from 'xlsx';
-import path, {parse} from "path";
+import path from "path";
 import moment from 'moment';
+import {common} from "../common.js";
 
 // this example reads the file synchronously
 // you can read it asynchronously also
 
 export class FlussoSIAD {
 
-    constructor() {
-
+    /**
+     * @param {ImpostazioniFlussoSIAD} settings - Settings
+     */
+    constructor(settings) {
+        this._settings = settings;
     }
 
-    contaPrestazioni (path) {
+    contaPrestazioni() {
 
         let data = {};
-        var totalePreseInCarico = 0;
-        var totaleAccessi = 0;
-        var totalePalliativa = 0;
-        const parser = new xml2js.Parser({ attrkey: "ATTR" });
+        const parser = new xml2js.Parser({attrkey: "ATTR"});
 
-        fs.readdir(path, (err, files) => {
-            files.forEach(file => {
-                console.log(file);
+        let files = common.getAllFilesRecursive(this._settings.in_folder, ".xml", "APS");
+        files.forEach(file => {
+            console.log(file);
 
-                let xml_string = fs.readFileSync(path + file, "utf8");
-
-                parser.parseString(xml_string, function (error, result) {
-                    if (error === null) {
-                        let assistenze = result['FlsAssDom_2']['Assistenza'];
-                        for (var i = 0; i < assistenze.length; i++) {
-                            let chiaveAssistito = assistenze[i]['Eventi'][0]['PresaInCarico'][0]['Id_Rec'][0];
-                            let assistito = chiaveAssistito.substr(chiaveAssistito.length - 16, chiaveAssistito.length - 1)
-                            if (!data.hasOwnProperty(assistito))
-                                data[assistito] = {'preseInCarico': 1, 'accessi': 0, 'palliativa': false};
-                            else {
-                                data[assistito]['preseInCarico'] = data[assistito]['preseInCarico'] + 1;
-                            }
-
-                            // accessi
-
-                            let accessi = assistenze[i]['Eventi'][0]['Erogazione'];
-                            if (accessi) {
-                                for (var k = 0; k < accessi.length; k++) {
-                                    if (assistenze[i]['Eventi'][0]['Erogazione'][k].hasOwnProperty('ATTR')) {
-                                        if (parseInt(assistenze[i]['Eventi'][0]['Erogazione'][k]['TipoOperatore'][0]) === 5) {
-                                            //palliativa?
-                                            data[assistito]['palliativa'] = true;
-                                        }
-                                        //console.log(accessi);
-                                        //console.log(assistenze[i]['Eventi'][0]['Erogazione'][k]['ATTR']['numAccessi']);
-                                        data[assistito]['accessi'] = data[assistito]['accessi'] + parseInt(assistenze[i]['Eventi'][0]['Erogazione'][k]['ATTR']['numAccessi']);
-                                    }
-                                }
-                            } else {
-                                //conclusione?
-                                //console.log(assistenze[i]['Eventi'][0]);
-                            }
+            let xml_string = fs.readFileSync(file, "utf8");
+            console.log("file:" + file)
+            parser.parseString(xml_string, function (error, result) {
+                if (error === null) {
+                    let assistenze = result['FlsAssDom_2']['Assistenza'];
+                    for (var i = 0; i < assistenze.length; i++) {
+                        let chiaveAssistito = assistenze[i]['Eventi'][0]['PresaInCarico'][0]['Id_Rec'][0];
+                        let assistito = chiaveAssistito.substr(chiaveAssistito.length - 16, chiaveAssistito.length - 1)
+                        if (!data.hasOwnProperty(assistito))
+                            data[assistito] = {'preseInCarico': 1, 'accessi': 0, 'palliativa': false};
+                        else {
+                            data[assistito]['preseInCarico'] = data[assistito]['preseInCarico'] + 1;
                         }
-                    } else {
-                        console.log(error);
+
+                        // accessi
+
+                        let accessi = assistenze[i]['Eventi'][0]['Erogazione'];
+                        if (accessi) {
+                            for (var k = 0; k < accessi.length; k++) {
+                                if (assistenze[i]['Eventi'][0]['Erogazione'][k].hasOwnProperty('ATTR')) {
+                                    if (parseInt(assistenze[i]['Eventi'][0]['Erogazione'][k]['TipoOperatore'][0]) === 5) {
+                                        //palliativa?
+                                        data[assistito]['palliativa'] = true;
+                                    }
+                                    //console.log(accessi);
+                                    //console.log(assistenze[i]['Eventi'][0]['Erogazione'][k]['ATTR']['numAccessi']);
+                                    data[assistito]['accessi'] = data[assistito]['accessi'] + parseInt(assistenze[i]['Eventi'][0]['Erogazione'][k]['ATTR']['numAccessi']);
+                                }
+                            }
+                        } else {
+                            //conclusione?
+                            //console.log(assistenze[i]['Eventi'][0]);
+                        }
                     }
-                });
+                } else {
+                    console.log(error);
+                }
+            });
 
 
-            })
-            console.log(data);
-            var chiavi = Object.keys(data);
-            for (var i = 0; i < chiavi.length; i++) {
-                //console.log(data[chiavi[i]])
+        })
+        //console.log(data);
+        var chiavi = Object.keys(data);
+        var totalePreseInCarico = 0;
+        var totalePreseIncaricoAlmenoUnAccesso = 0;
+        var totalePalliativa = 0;
+        var totaleAccessiPalliativa = 0;
+        var totaleAccessiGeriatrica = 0;
+
+        for (var i = 0; i < chiavi.length; i++) {
+            //console.log(data[chiavi[i]])
+            if (data[chiavi[i]]['accessi'] > 0) {
+                totalePreseIncaricoAlmenoUnAccesso++;
                 totalePreseInCarico += data[chiavi[i]]['preseInCarico'];
-                totaleAccessi += data[chiavi[i]]['accessi'];
-                if (data[chiavi[i]]['palliativa'] === true)
+                if (data[chiavi[i]]['palliativa'] === true) {
                     totalePalliativa++;
+                    totaleAccessiPalliativa += data[chiavi[i]]['accessi'];
+                }
+                else
+                    totaleAccessiGeriatrica += data[chiavi[i]]['accessi'];
             }
-            console.log(Object.keys(data).length);
-            console.log(totalePreseInCarico);
-            console.log(totaleAccessi);
-            console.log(totalePalliativa);
-        });
+        }
+
+        console.log("Totale prese in carico : " + chiavi.length);
+        console.log("Totale prese in carico almeno un accesso: " + totalePreseIncaricoAlmenoUnAccesso);
+        console.log("Totali geriatrica: " + (totalePreseIncaricoAlmenoUnAccesso- totalePalliativa));
+        console.log("Totali palliativa: " + totalePalliativa);
+        console.log("Totali accessi: " + (totaleAccessiPalliativa + totaleAccessiGeriatrica));
+        console.log("Totali accessi Geriatrica: " + totaleAccessiGeriatrica);
+        console.log("Totali accessi Palliativa: " + totaleAccessiPalliativa);
     }
 
-    generaFlussoRettificaChiusure(pathFile,folderOut,codRegione, codASL)
-    {
+    generaFlussoRettificaChiusure(pathFile, folderOut, codRegione, codASL) {
         //objRettifica = [{chiave: xx, pic:xxx, conclusione: xxx, motivazione: xxx} .... ]
         // Reading our test file
         const file = reader.readFile(pathFile);
@@ -101,22 +116,21 @@ export class FlussoSIAD {
             });
         }
         console.log(data[0]);
-        let i = 0 ;
+        let i = 0;
         let k = 0;
         let a2021 = 0;
         let outData = {};
         let chiavi = {}
-        for(let dato of data)
-        {
+        for (let dato of data) {
             if (dato["Data Conclusione"].startsWith("--")) {
                 let annoPIC = parseInt(dato["Anno Presa In Carico"]);
                 let annoRivalutazione = !dato["Ultima Data Rivalutazione "].startsWith("--") ? parseInt(dato["Ultima Data Rivalutazione "].substring(0, 4)) : annoPIC;
                 let annoUltimaErogazione = !dato["Ultima Data Erogazione"].startsWith("--") ? parseInt(dato["Ultima Data Erogazione"].substring(0, 4)) : annoPIC
                 let annoFineSospensione = dato.hasOwnProperty("Data Fine Sospensione") ? (!dato["Data Fine Sospensione"].startsWith("--") ? parseInt(dato["Data Fine Sospensione"].substring(0, 4)) : 0) : annoPIC;
                 let anno = Math.max(annoPIC, annoRivalutazione, annoUltimaErogazione, annoFineSospensione)
-                if (anno >2013 && anno <2022) {
+                if (anno > 2013 && anno < 2022) {
                     console.log("Elaboro record " + ++i)
-                    if (anno === 2021) a2021 ++;
+                    if (anno === 2021) a2021++;
                     //console.log(dato);
                     let tempRiga = {
                         Trasmissione: {$: {"tipo": "I"}},
@@ -135,29 +149,25 @@ export class FlussoSIAD {
                     if (!outData.hasOwnProperty(anno))
                         outData[anno] = [];
                     outData[anno].push(tempRiga);
-                }
-                else
+                } else
                     console.log("Record non elaborato " + ++k)
-            }
-            else
+            } else
                 console.log("Record non elaborato " + ++k)
         }
 
         var builder = new xml2js.Builder();
-        for (let chiave of Object.keys(outData))
-        {
-            var obj = {FlsAssDom_2: {$: {"xmlns": "http://flussi.mds.it/flsassdom_2"},Assistenza: outData[chiave]}}
+        for (let chiave of Object.keys(outData)) {
+            var obj = {FlsAssDom_2: {$: {"xmlns": "http://flussi.mds.it/flsassdom_2"}, Assistenza: outData[chiave]}}
             var xml = builder.buildObject(obj);
-            fs.writeFileSync(folderOut + path.sep + codRegione + codASL + "_000_" + chiave.substring(0,4) + "_12_SIAD_APS_al_" + moment().date() + "_" + ((moment().month() +1) <10 ? ("0" + (moment().month() +1)) : (moment().month() +1)) + "_" + moment().year() +   ".xml", xml);
+            fs.writeFileSync(folderOut + path.sep + codRegione + codASL + "_000_" + chiave.substring(0, 4) + "_12_SIAD_APS_al_" + moment().date() + "_" + ((moment().month() + 1) < 10 ? ("0" + (moment().month() + 1)) : (moment().month() + 1)) + "_" + moment().year() + ".xml", xml);
         }
-        console.log("Chiusi: " + i + " - Non elaborati: " + k )
+        console.log("Chiusi: " + i + " - Non elaborati: " + k)
         console.log("2021: " + a2021)
         //console.log(xml);
 
     }
 
-    generaFlussoRettificaCancellazione(pathFile,folderOut,codRegione, codASL)
-    {
+    generaFlussoRettificaCancellazione(pathFile, folderOut, codRegione, codASL) {
         const file = reader.readFile(pathFile);
 
         let data = [];
@@ -175,18 +185,18 @@ export class FlussoSIAD {
         console.log(data[0]);
 
         let outData = {};
-        for(let dato of data)
-        {
+        for (let dato of data) {
             let annoPIC = dato["Anno Presa In Carico"];
-            let annoUltimaErogazione = dato["Ultima Data Erogazione"].length > 2 ? dato["Ultima Data Erogazione"].substring(0,4) : "0"
+            let annoUltimaErogazione = dato["Ultima Data Erogazione"].length > 2 ? dato["Ultima Data Erogazione"].substring(0, 4) : "0"
             let dataConclusione = dato["Data Conclusione"].length > 2 ? dato["Data Conclusione"] : "0"
-            let mesePic = dato["Data  Presa In Carico"].length > 2 ? dato["Data  Presa In Carico"].substring(5,7) : "0"
+            let mesePic = dato["Data  Presa In Carico"].length > 2 ? dato["Data  Presa In Carico"].substring(5, 7) : "0"
             if (mesePic.length === 1) mesePic = "0" + mesePic;
             //console.log(dato);
-            let tempRiga = {Trasmissione: {$: {"tipo":"C"}},
+            let tempRiga = {
+                Trasmissione: {$: {"tipo": "C"}},
                 Assistito: {
                     DatiAnagrafici: {
-                        CUNI:  dato["Id Record"].substring(16,32),
+                        CUNI: dato["Id Record"].substring(16, 32),
                         validitaCI: 0,
                         tipologiaCI: 0,
                         AnnoNascita: 1950,
@@ -204,7 +214,7 @@ export class FlussoSIAD {
                     NucleoFamiliare: 0,
                     AssistenteNonFamiliare: 1,
                 },
-                Erogatore: {CodiceRegione: codRegione,CodiceASL:codASL},
+                Erogatore: {CodiceRegione: codRegione, CodiceASL: codASL},
                 Eventi: {
                     PresaInCarico: {
                         $: {"data": dato["Data  Presa In Carico"], "soggettoRichiedente": 2},
@@ -261,18 +271,17 @@ export class FlussoSIAD {
                 }
             };
             if (annoUltimaErogazione === "0") {
-                if (!outData.hasOwnProperty(annoPIC+ "_" + mesePic))
-                    outData[annoPIC+ "_" + mesePic] = [];
-                outData[annoPIC+ "_" + mesePic].push(tempRiga);
+                if (!outData.hasOwnProperty(annoPIC + "_" + mesePic))
+                    outData[annoPIC + "_" + mesePic] = [];
+                outData[annoPIC + "_" + mesePic].push(tempRiga);
             }
         }
 
         var builder = new xml2js.Builder();
-        for (let chiave of Object.keys(outData))
-        {
-            var obj = {FlsAssDom_1: {$: {"xmlns": "http://flussi.mds.it/flsassdom_1"},Assistenza: outData[chiave]}}
+        for (let chiave of Object.keys(outData)) {
+            var obj = {FlsAssDom_1: {$: {"xmlns": "http://flussi.mds.it/flsassdom_1"}, Assistenza: outData[chiave]}}
             var xml = builder.buildObject(obj);
-            fs.writeFileSync(folderOut + path.sep + codRegione + codASL + "_000_" + chiave.substring(0,4) + "_" + chiave.substring(5,7) +"_SIAD_AAD_al_" + moment().date() + "_" + ((moment().month() +1) <10 ? ("0" + (moment().month() +1)) : (moment().month() +1)) + "_" + moment().year() +   ".xml", xml);
+            fs.writeFileSync(folderOut + path.sep + codRegione + codASL + "_000_" + chiave.substring(0, 4) + "_" + chiave.substring(5, 7) + "_SIAD_AAD_al_" + moment().date() + "_" + ((moment().month() + 1) < 10 ? ("0" + (moment().month() + 1)) : (moment().month() + 1)) + "_" + moment().year() + ".xml", xml);
         }
 
         //console.log(xml);
